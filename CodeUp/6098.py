7f92d52c3c0bb092d76c363d788d8488aa80e351
sharknia <furychick0@gmail.com>
2021-04-26 20:25:07 +0900

    개미 끝!

--- CodeUp/6098.py
+++ CodeUp/6098.py
@@ -11,16 +11,19 @@ y = 1
 
 while True:
     if d[x][y] == 2 :
+        d[x][y] = 9
         break
-    d[x][y] = 9
     #오른쪽이 0이라면 이동한다.
     #오른쪽이 0이 아니고 아래가 0이라면 이동한다 .
     #둘 다 아니라면 멈춘다.
-    if x < 9 and d[x+1][y] == 0:
+    if y < 8 and (d[x][y+1] == 0 or d[x][y+1] == 2):
+        d[x][y] = 9
         y += 1
-    elif y < 9 and d[x][y+1] == 0:
+    elif x < 8 and (d[x+1][y] == 0 or d[x+1][y] == 2):
+        d[x][y] = 9
         x += 1
     else:
+        d[x][y] = 9
         break
 
 for i in range(10) : 
